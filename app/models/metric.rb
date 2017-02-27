9282787ff63868c2e9cc80f87a012b993e6fdf9c
Nick Benoit <nick.benoit14@gmail.com>
2017-02-26 19:48:53 -0700

    begins metric validation

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,3 +1,14 @@
 class Metric < ApplicationRecord
   belongs_to :Event
+  METRIC_TYPES = {
+    integer: "INTEGER",
+    boolean: "BOOLEAN",
+    time: "TIME"
+  }
+
+  validates :type, inclusion: METRIC_TYPES
+
+  def types
+    METRIC_TYPES
+  end
 end
--- test/models/metric_test.rb
+++ test/models/metric_test.rb
@@ -4,4 +4,10 @@ class MetricTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "validates" do
+    good = Metric.new(type: Metric.types[:integer])
+    bad = Metric.new(type: "nonsense")
+    # TODO figure out test throws with minitest
+  end
 end
