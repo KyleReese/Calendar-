06172336105a9c801dec4e2389476707647a9328
Nick Benoit <nick.benoit14@gmail.com>
2017-02-27 21:11:03 -0700

    remove byebug

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,5 @@
 class CallbacksController < ApplicationController
   def google_oauth2
-    byebug
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
