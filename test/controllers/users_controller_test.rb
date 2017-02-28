c3958637e0ffa6d7c1dd2ebe5c9fd13381e379f9
Nick Benoit <nick.benoit14@gmail.com>
2017-02-27 21:20:04 -0700

    adds index route

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "get#index" do
+    get "/users"
+    assert_equal 200, status
+  end
+
+  
 end
