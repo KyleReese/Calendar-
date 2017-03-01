7cacc2d2e9f2de91b3dad89d18f9ced0cb0b3b8c
jaydenhthompson <jaydenhthompson@gmail.com>
2017-02-28 20:37:18 -0700

    Changed event html files, added methods and a basis for html structure. (#2)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,19 @@
-class EventController < ApplicationController
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy!
+    redirect_to events_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   resources :users
   root to: "home#index"
+
+  resources :events
 end
