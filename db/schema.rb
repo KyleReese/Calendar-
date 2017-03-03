01e70c601c967baa37ebf1157c314cd8fed9ae46
jaydenhthompson <jaydenhthompson@gmail.com>
2017-03-02 19:48:01 -0700

    Started the Event Metric views and edit.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303013016) do
+ActiveRecord::Schema.define(version: 20170303023910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170303013016) do
     t.datetime "updated_at",        null: false
     t.string   "event_calendar_id"
     t.integer  "metric_id"
+    t.text     "name"
     t.index ["metric_id"], name: "index_events_on_metric_id", using: :btree
   end
 
