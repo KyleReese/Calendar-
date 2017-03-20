30c120ce694f6ffab7e566ca1d79950919065abb
Zenaido <luis.hernandez@aggiemail.usu.edu>
2017-03-20 12:26:03 -0600

    minor changes delete unused views that i added last time that messed up other routes,

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/redirect', to: 'events#redirect', as: 'redirect'
-  get '/callback', to: 'events#callback', as: 'callback'
-  get '/calendars', to: 'events#calendars', as: 'calendars'
-  get '/events/:calendar_id', to: 'events#google', as: 'google', calendar_id: /[^\/]+/
 
   resources :users
   root to: "home#index"
