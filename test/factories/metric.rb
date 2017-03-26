06d843102066bc1d137b860cd55ecde1178cf2df
Nick Benoit <nick.benoit14@gmail.com>
2017-03-25 19:46:13 -0600

    fix metric factory

--- test/factories/metric.rb
+++ test/factories/metric.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
-  factory :int_metric, class Metric do
+  factory :int_metric, class: Metric do
     int_val 4
     metric_type Metric.types[:integer]
   end
 
-  factory :bool_metric, class Metric do
+  factory :bool_metric, class: Metric do
     bool_val true
     metric_type Metric.types[:boolean]
   end
 
-  factory :time_metric, class Metric do
+  factory :time_metric, class: Metric do
     time_val Time.new(2000)
     metric_type Metric.types[:time]
   end
