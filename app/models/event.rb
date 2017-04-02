d3fa96e493c99d154407f5be39a6d25902a5946e
Nick Benoit <nick.benoit14@gmail.com>
2017-04-01 18:14:30 -0600

    unique metric classes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   has_many :event_event_class
   has_many :event_classes, through: :event_event_class
-  has_many :metric_classes, through: :event_classes
+  has_many :metric_classes, -> {distinct}, through: :event_classes
   has_many :metrics
 end
