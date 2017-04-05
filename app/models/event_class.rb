afecec74cf81e89902243011bf93e660e0d173c8
Nick Benoit <nick.benoit14@gmail.com>
2017-04-04 21:33:03 -0600

    working event class edit

--- app/models/event_class.rb
+++ app/models/event_class.rb
@@ -2,4 +2,12 @@ class EventClass < ApplicationRecord
   has_many :event_event_class
   has_many :events, through: :event_event_class
   has_many :metric_classes
+
+
+  def update_metric_classes(updated_metrics_classes)
+    updated_metrics_classes.keys.each do |id|
+      metric_class = MetricClass.find(id)
+      metric_class.update(metric_type: updated_metrics_classes[id][:metric_type])
+    end
+  end
 end
