bc225381dc312f6fe16dc49ba274dad50c406ee6
Nick Benoit <nick.benoit14@gmail.com>
2017-04-05 20:38:58 -0600

    add basic report

--- app/controllers/metric_classes_controller.rb
+++ app/controllers/metric_classes_controller.rb
@@ -1,5 +1,13 @@
 class MetricClassesController < ApplicationController
   def show
     @metric_class = MetricClass.find(params[:id])
+    @data = case @metric_class.metric_type
+    when MetricClass.types[:integer]
+      @metric_class.metrics.map(&:int_val)
+    when MetricClass.types[:boolean]
+      @metric_class.metrics.map(&:boolean_val)
+    when MetricClass.types[:time]
+      @metric_class.metrics.map(&:time_val)
+    end
   end
 end
