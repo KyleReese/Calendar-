296f8e870e64f7306b80c2eef425c0ee8f2a9c56
Nick Benoit <nick.benoit14@gmail.com>
2017-04-05 22:00:05 -0600

    basic chart

--- app/controllers/metric_classes_controller.rb
+++ app/controllers/metric_classes_controller.rb
@@ -3,11 +3,11 @@ class MetricClassesController < ApplicationController
     @metric_class = MetricClass.find(params[:id])
     @data = case @metric_class.metric_type
     when MetricClass.types[:integer]
-      @metric_class.metrics.map(&:int_val)
+      @metric_class.metrics.map { |m| {id: m.id, name: m.metric_class.name, val: m.int_val}}
     when MetricClass.types[:boolean]
-      @metric_class.metrics.map(&:boolean_val)
+      @metric_class.metrics.map { |m| {id: m.id, name: m.metric_class.name, val: m.boolean_val}}
     when MetricClass.types[:time]
-      @metric_class.metrics.map(&:time_val)
+      @metric_class.metrics.map { |m| {id: m.id, name: m.metric_class.name, val: m.time_val}}
     end
   end
 end
