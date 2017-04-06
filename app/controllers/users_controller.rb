d0062e591e047c6bddb24779698e3822c12dadb0
jaydenhthompson <jaydenhthompson@gmail.com>
2017-04-06 17:22:49 -0600

    Jaydens branch (#21)
    * Added a send_mail method
    
    * Changed stuff.
    
    * Got rid of sqlite install for heroku push
    
    * Updated the Gemfile.lock
    
    * Added Notifications slider
    
    * Adds notification options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    redirect_to @user
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy!
     redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:notificationoption)
   end
 end
